64fd43c814436eed4d9f7211ed9411d0b15e2acc
brelee2222 <brelee1842@outlook.com>
2024-06-27 10:40:54 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const COLORS = {
+    "red" : "red",
+    "blue" : "blue",
+    "white" : "white",
+    "green" : "green",
+    "orange" : "orange",
+    "yellow" : "yellow"
+};
+
+const PIECE_TYPES = [
+    "middle", // 1 face
+    "edge", // 2 faces
+    "corner" // 3 faces
+];
+
+const SCENE = document.getElementById("main-scene");
+const CUBE_TEMPLATE = document.getElementById("cubePiece").content;
+
+class CubePiece {
+    constructor() {
+        
+    }
+}
